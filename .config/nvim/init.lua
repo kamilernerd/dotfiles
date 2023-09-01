169d104a08591c4df30a0e49bb48883571c0ace6
Kamil <kamil.oracz98@gmail.com>
2023-09-01 11:55:08 +0200

    Revert back startup config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -187,6 +187,38 @@ require('lazy').setup({
           default_color = "",
           oldfiles_amount = 0,
         },
+        footer = {
+          type = "text",
+          oldfiles_directory = false,
+          align = "center",
+          fold_section = false,
+          title = "Footer",
+          margin = 5,
+          content = { "" },
+          highlight = "Number",
+          default_color = "",
+          oldfiles_amount = 0,
+        },
+
+        options = {
+          mapping_keys = true,
+          cursor_column = 0.5,
+          empty_lines_between_mappings = true,
+          disable_statuslines = true,
+          paddings = { 1, 3, 3, 0 },
+        },
+        mappings = {
+          execute_command = "<CR>",
+          open_file = "o",
+          open_file_split = "<c-o>",
+          open_section = "<TAB>",
+          open_help = "?",
+        },
+        colors = {
+          background = "#1f2227",
+          folded_section = "#56b6c2",
+        },
+        parts = { "header", "body", "footer" },
       })
     end
   },
