b9cddc66601e132735958f8f3dd74019bea2a3ef
Kamil Oracz <kamil.oracz98@gmail.com>
2024-02-27 00:09:18 +0100

    Improve lsp-config

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -4,12 +4,12 @@ return {
     lazy = false,
     config = function()
       require("mason").setup({
-				ensure_installed = vim.tbl_keys({
-					gopls = {},
-					tsserver = {},
-					rust_analyzer = {},
-				})
-			})
+	ensure_installed = vim.tbl_keys({
+	  gopls = {},
+	  tsserver = {},
+	  rust_analyzer = {},
+	})
+      })
     end,
   },
   {
@@ -37,24 +37,26 @@ return {
       })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-  		vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, {})
+      vim.keymap.set("n", "gDD", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gII", vim.lsp.buf.implementation, {})
+      vim.keymap.set("n", "grr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "caa", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "gdd", vim.lsp.buf.declaration, {})
+      vim.keymap.set("n", ",,", vim.diagnostic.open_float, {})
 
-			-- On cursor hover documentation/diagnostics
-			vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float()]]
-			vim.cmd [[autocmd CursorHoldI * silent! lua vim.buf.signature_help()]]
+      -- On cursor hover documentation/diagnostics
+      -- vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float()]]
+      vim.cmd [[autocmd CursorHoldI * silent! lua vim.buf.signature_help()]]
 
-			-- Disable annoying lsp messages at the EOL
-			vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-				vim.lsp.diagnostic.on_publish_diagnostics, {
-					virtual_text = false,
-					underline = true,
-					signs = true,
-				}
-			)
+      -- Disable annoying lsp messages at the EOL
+      vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+	  virtual_text = false,
+	  underline = false,
+	  signs = true,
+	}
+      )
     end,
   },
 }
