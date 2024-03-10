b83845821e1462f4719516e916e0ad8e02c5ddad
Kamil Oracz <kamil.oracz98@gmail.com>
2024-03-11 00:33:34 +0100

    Add installation script and wallpapers

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
-## Installation
- Requirements
- - Have tmux installed
- - Have neovim installed
+### Installation
+Requirements
+    - i3wm (Window manager)
+    - tmux (Terminal multiplexer)
+    - neovim (Text editor)
+    - arandr (Nice screen configurator)
+    - feh (Terminal image viewer)
+    - polybar (Topbar)
+    - nitrogen (Wallpapers)
+    - Alacritty (Terminal emulator)
+    - xfce4-power-manager (Power manager with cool options)
+    - nm-applet (Wifi manager)
+    - zsh (Shell)
+    - pulseaudio (If not included in the distro)
 
-Unpack in the home directory
-
+### Unpack in the home directory
 Install fots on the system
  https://sourcefoundry.org/hack/ (Enable it in the terminal)
  https://www.nerdfonts.com/font-downloads (Enable "Use a different font for
@@ -13,17 +22,8 @@ non-ASCII text, then select nerd and enable use-ligatures")
 Open tmux session
 ```ctrl + b + I (Capital i)```
 
-Install nodejs 
- ```npm install -g neovim```
-
-Install python
-
-(On mac)
- ```/opt/homebrew/bin/pip3 install --user --upgrade neovim```
-
-(On debian/ubuntu)
- ```sudo apt-get install python3-neovim```
-
 Open nvim
  ```:Lazy```
  ```:Mason```
+
+Run ```./install.sh install```
---
+++ install.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+declare -a dirs=("i3" "polybar" "nvim")
+
+CONFIG_DIR=$HOME/.config
+
+delete_current_configuration() {
+	for i in "${dirs[@]}"
+	do
+		echo "Deleting $HOME/$i"
+		rm -r "$CONFIG_DIR/$i"
+	done
+
+	echo "Deleting $HOME/.tmux.conf"
+	rm $HOME/.tmux.conf
+}
+
+install_configuration() {
+	for i in "${dirs[@]}"
+	do
+		echo "Installing $PWD/$i in path $CONFIG_DIR/$i"
+		cp -r $PWD/$i $CONFIG_DIR/$i
+	done
+
+	echo "Installing wallpapers to $HOME/.local/share/backgrounds"
+	mkdir -p $HOME/.local/share/backgrounds
+	cp $PWD/wallpapers/* $HOME/.local/share/backgrounds
+
+	echo "Installing .tmux.conf in path $HOME/.tmux.conf"
+	cp $PWD/.tmux.conf $HOME/.tmux.conf
+}
+
+reinstall() {
+	echo "Staring..."
+	delete_current_configuration
+	install_configuration
+	echo "Done."
+}
+
+help() {
+	echo "Usage: ./install.sh [install|remove|reinstall|help]"
+	echo "  install   - Installs dotfiles"
+	echo "  remove    - Uninstalls dotfiles"
+	echo "  reinstall - Reinstalls dotfiles"
+	echo "  help      - Shows this page"
+}
+
+for arg in "$@"
+do
+		case "${arg}" in
+				install) install_configuration;;
+				remove) delete_current_configuration;;
+				reinstall) reinstall;;
+				help) help;;
+				*) echo "Invalid option: $arg" && help;;
+		esac
+done
